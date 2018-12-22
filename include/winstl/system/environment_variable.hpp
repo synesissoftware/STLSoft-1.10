784ca953264eeb681f4a874f229a47f4c322b00f
Matt Wilson <matthew@synesis.com.au>
2018-12-22 16:22:32 +1100

    winstl::environment_variable : + data(); + equal(); + equal_ignore_case(); + exists(); + operators ==() and !=()

--- include/winstl/system/environment_variable.hpp
+++ include/winstl/system/environment_variable.hpp
@@ -4,13 +4,13 @@
  * Purpose:     Simple class that provides access to an environment variable.
  *
  * Created:     20th December 2002
- * Updated:     22nd August 2017
+ * Updated:     21st December 2018
  *
  * Thanks to:   Pablo Aguilar for requesting size() and empty().
  *
  * Home:        http://stlsoft.org/
  *
- * Copyright (c) 2002-2017, Matthew Wilson and Synesis Software
+ * Copyright (c) 2002-2018, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -53,9 +53,9 @@
 
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_MAJOR    4
-# define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_MINOR    3
-# define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_REVISION 7
-# define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_EDIT     77
+# define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_MINOR    4
+# define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_REVISION 1
+# define WINSTL_VER_WINSTL_SYSTEM_HPP_ENVIRONMENT_VARIABLE_EDIT     80
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -75,9 +75,6 @@
 #ifndef STLSOFT_INCL_STLSOFT_SHIMS_ACCESS_HPP_STRING
 # include <stlsoft/shims/access/string.hpp>
 #endif /* !STLSOFT_INCL_STLSOFT_SHIMS_ACCESS_HPP_STRING */
-#ifndef WINSTL_INCL_WINSTL_SHIMS_ACCESS_HPP_STRING
-# include <winstl/shims/access/string.hpp>
-#endif /* !WINSTL_INCL_WINSTL_SHIMS_ACCESS_HPP_STRING */
 #ifndef STLSOFT_INCL_STLSOFT_MEMORY_HPP_AUTO_BUFFER
 # include <stlsoft/memory/auto_buffer.hpp>
 #endif /* !STLSOFT_INCL_STLSOFT_MEMORY_HPP_AUTO_BUFFER */
@@ -129,57 +126,44 @@ class basic_environment_variable
 {
 public:
     /// The char type
-    typedef C                                   char_type;
+    typedef C                                               char_type;
     /// The traits type
-    typedef T                                   traits_type;
+    typedef T                                               traits_type;
     /// The allocator type
-    typedef processheap_allocator<C>            allocator_type;
+    typedef processheap_allocator<C>                        allocator_type;
     /// The current parameterisation of the type
-    typedef basic_environment_variable<C, T>    class_type;
+    typedef basic_environment_variable<C, T>                class_type;
     /// The size type
-    typedef ws_size_t                           size_type;
+    typedef ws_size_t                                       size_type;
     /// The difference type
-    typedef ws_ptrdiff_t                        difference_type;
+    typedef ws_ptrdiff_t                                    difference_type;
     /// The Boolean type
-    typedef ws_bool_t                           bool_type;
+    typedef ws_bool_t                                       bool_type;
+private:
+    typedef STLSOFT_NS_QUAL(auto_buffer_old)<
+        char_type
+    ,   allocator_type
+    >                                                       buffer_type_;
 
 // Construction
 public:
     /// Create an instance representing the given environment variable
     ss_explicit_k basic_environment_variable(char_type const* name)
-        : m_buffer(1 + traits_type::get_environment_variable(name, NULL, 0))
-    {
-        if( 0 == traits_type::get_environment_variable(name, &m_buffer[0], m_buffer.size()) &&
-            0 != m_buffer.size())
-        {
-            m_buffer[0] = 0;
-        }
-        else
-        {
-            // This is necessary because GetEnvironmentVariable() does not do what
-            // it's documented to do
-            m_buffer.resize(1 + traits_type::str_len(m_buffer.data()));
-        }
-    }
+        : m_buffer(1)
+        , m_exists(obtain_(STLSOFT_NS_QUAL(c_str_ptr)(name), m_buffer))
+    {}
 #ifdef STLSOFT_CF_MEMBER_TEMPLATE_CTOR_SUPPORT
+
     /// Create an instance representing the given environment variable
     template<ss_typename_param_k S>
     ss_explicit_k basic_environment_variable(S const& name)
-        : m_buffer(1 + traits_type::get_environment_variable(STLSOFT_NS_QUAL(c_str_ptr)(name), NULL, 0))
-    {
-        if( 0 == traits_type::get_environment_variable(STLSOFT_NS_QUAL(c_str_ptr)(name), &m_buffer[0], m_buffer.size()) &&
-            0 != m_buffer.size())
-        {
-            m_buffer[0] = 0;
-        }
-        else
-        {
-            // This is necessary because GetEnvironmentVariable() does not do what
-            // it's documented to do
-            m_buffer.resize(1 + traits_type::str_len(m_buffer.data()));
-        }
-    }
+        : m_buffer(1)
+        , m_exists(obtain_(STLSOFT_NS_QUAL(c_str_ptr)(name), m_buffer))
+    {}
 #endif /* STLSOFT_CF_MEMBER_TEMPLATE_CTOR_SUPPORT */
+private:
+    basic_environment_variable(class_type const&);  // copy-construction proscribed
+    class_type& operator =(class_type const&);      // copy-assignment proscribed
 
 // Conversions
 public:
@@ -189,14 +173,20 @@ public:
         return m_buffer.data();
     }
 
+public: // accessors
     /// Returns a pointer to a nul-terminated string
     char_type const* c_str() const
     {
         return m_buffer.data();
     }
 
-// Attributes
-public:
+    /// A non-null pointer to the stored characters
+    char_type const*
+    data() const
+    {
+        return m_buffer.data();
+    }
+
     /// Returns the length of the variable
     size_type length() const
     {
@@ -207,23 +197,165 @@ public:
     {
         return m_buffer.size() - 1;
     }
+    /// Indicate whether the environment variable is empty
+    ///
+    /// \note If the variable does not exist, this method will
+    ///   true. Use exists()
+    ///
+    /// \see exists()
     bool_type empty() const
     {
         return 0u == size();
     }
+    /// Indicates whether the environment variable exists
+    bool_type exists() const
+    {
+        return m_exists;
+    }
 
-// Members
+public: // comparison
+    /// Determines whether the given string contains the same value
+    template <ss_typename_param_k T_string>
+    bool
+    equal(
+        T_string const&   s
+    ) const
+    {
+        return equal_n_(
+            this->data(), this->size()
+        ,   stlsoft::c_str_data(s), stlsoft::c_str_len(s)
+        );
+    }
+
+    /// Determines whether the given instance contains the same value
+    bool
+    equal(class_type const& rhs) const
+    {
+        if(rhs.size() != size())
+        {
+            return false;
+        }
+
+        return 0 == traits_type::str_n_compare(data(), rhs.data(), rhs.size());
+    }
+
+    /// Determines whether the given string contains the same value, ignoring case
+    template <ss_typename_param_k T_string>
+    bool
+    equal_ignore_case(
+        T_string const&   s
+    ) const
+    {
+        return equal_n_ignore_case_(
+            this->data(), this->size()
+        ,   stlsoft::c_str_data(s), stlsoft::c_str_len(s)
+        );
+    }
+
+private: // implementation
+    bool
+    equal_n_(
+        char_type const*  s1
+    ,   size_type         cch1
+    ,   char_type const*  s2
+    ,   size_type         cch2
+    ) const
+    {
+        if(cch1 != cch2)
+        {
+            return false;
+        }
+
+        return 0 == traits_type::str_n_compare(s1, s2, cch2);
+    }
+
+    bool
+    equal_n_ignore_case_(
+        char_type const*  s1
+    ,   size_type         cch1
+    ,   char_type const*  s2
+    ,   size_type         cch2
+    ) const
+    {
+        if(cch1 != cch2)
+        {
+            return false;
+        }
+
+        return 0 == traits_type::str_n_compare_no_case(s1, s2, cch2);
+    }
+
+    static
+    bool_type
+    obtain_(
+        char_type const*     name
+    ,   buffer_type_&       buffer
+    )
+    {
+        WINSTL_ASSERT(1 == buffer.size());
+
+        WINSTL_API_EXTERNAL_ErrorHandling_SetLastError(0);
+
+        size_type const n1  = traits_type::get_environment_variable(name, NULL, 0);
+
+        if(ERROR_ENVVAR_NOT_FOUND == WINSTL_API_EXTERNAL_ErrorHandling_GetLastError())
+        {
+            buffer[0] = 0;
+
+            return false;
+        }
+        else
+        {
+            if(0 == n1)
+            {
+                buffer[0] = 0;
+
+                return true;
+            }
+            else
+            {
+                if(!buffer.resize(1 + n1))
+                {
+                    WINSTL_API_EXTERNAL_ErrorHandling_SetLastError(ERROR_OUTOFMEMORY);
+
+                    return false;
+                }
+                else
+                {
+                    size_type const n2 = traits_type::get_environment_variable(name, &buffer[0], buffer.size());
+
+                    if(ERROR_ENVVAR_NOT_FOUND == WINSTL_API_EXTERNAL_ErrorHandling_GetLastError())
+                    {
+                        buffer[0] = 0;
+
+                        return false;
+                    }
+                    else
+                    {
+                        if(0 == n2)
+                        {
+                            buffer[0] = 0;
+
+                            return true;
+                        }
+                        else
+                        {
+                            // This is necessary because GetEnvironmentVariable() does not do what
+                            // it's documented to do
+                            buffer.resize(1 + traits_type::str_len(buffer.data()));
+
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+// fields
 private:
-    typedef STLSOFT_NS_QUAL(auto_buffer_old)<   char_type
-                                            ,   allocator_type
-                                            >       buffer_type_;
-
     buffer_type_    m_buffer;
-
-// Not to be implemented
-private:
-    basic_environment_variable(basic_environment_variable const&);
-    basic_environment_variable& operator =(basic_environment_variable const&);
+    bool_type       m_exists;
 };
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -245,6 +377,98 @@ typedef basic_environment_variable<ws_char_w_t, system_traits<ws_char_w_t> >    
  * \ingroup group__library__System
  */
 typedef basic_environment_variable<TCHAR, system_traits<TCHAR> >                environment_variable;
+
+/* /////////////////////////////////////////////////////////////////////////
+ * operators
+ */
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k S
+>
+inline
+bool
+operator ==(
+    basic_environment_variable<C, T> const& lhs
+,   S const&                                rhs
+)
+{
+    return lhs.equal(rhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k S
+>
+inline
+bool
+operator !=(
+    basic_environment_variable<C, T> const& lhs
+,   S const&                                rhs
+)
+{
+    return !lhs.equal(rhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k S
+>
+inline
+bool
+operator ==(
+    S const&                                lhs
+,   basic_environment_variable<C, T> const& rhs
+)
+{
+    return rhs.equal(lhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k S
+>
+inline
+bool
+operator !=(
+    S const&                                lhs
+,   basic_environment_variable<C, T> const& rhs
+)
+{
+    return !rhs.equal(lhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+>
+inline
+bool
+operator ==(
+    basic_environment_variable<C, T> const& lhs
+,   basic_environment_variable<C, T> const& rhs
+)
+{
+    return rhs.equal(lhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+>
+inline
+bool
+operator !=(
+    basic_environment_variable<C, T> const& lhs
+,   basic_environment_variable<C, T> const& rhs
+)
+{
+    return !rhs.equal(lhs);
+}
 
 /* /////////////////////////////////////////////////////////////////////////
  * helper functions
