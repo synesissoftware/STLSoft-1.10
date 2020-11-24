7beeee0cfb9ec616974414eac8fc4a7cb37be358
Matt Wilson <matthew@synesis.com.au>
2020-11-25 05:14:51 +1100

    + added move constructor for (basic_)simple_string<>

--- include/stlsoft/string/simple_string.hpp
+++ include/stlsoft/string/simple_string.hpp
@@ -4,10 +4,11 @@
  * Purpose:     basic_simple_string class template.
  *
  * Created:     19th March 1993
- * Updated:     13th September 2019
+ * Updated:     24th November 2020
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 1993-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -20,9 +21,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -51,9 +53,9 @@
 
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_MAJOR    4
-# define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_MINOR    2
-# define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_REVISION 7
-# define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_EDIT     263
+# define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_MINOR    3
+# define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_REVISION 1
+# define STLSOFT_VER_STLSOFT_STRING_HPP_SIMPLE_STRING_EDIT     264
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -261,6 +263,13 @@ public:
         assign(first, last);
     }
 #endif /* STLSOFT_CF_MEMBER_TEMPLATE_RANGE_METHOD_SUPPORT */
+
+#ifdef STLSOFT_CF_RVALUE_REFERENCES_SUPPORT
+
+    /// Transfers the contents of the instance \c rhs
+    basic_simple_string(class_type&& rhs) STLSOFT_NOEXCEPT;
+#endif /* STLSOFT_CF_RVALUE_REFERENCES_SUPPORT */
+
     /// Destructor
     ~basic_simple_string() STLSOFT_NOEXCEPT;
 /// @}
@@ -1627,6 +1636,20 @@ inline basic_simple_string<C, T, A>::basic_simple_string(   ss_typename_type_k b
     STLSOFT_ASSERT(is_valid());
 }
 #endif /* !STLSOFT_CF_MEMBER_TEMPLATE_RANGE_METHOD_SUPPORT */
+
+#ifdef STLSOFT_CF_RVALUE_REFERENCES_SUPPORT
+
+template<   ss_typename_param_k C
+        ,   ss_typename_param_k T
+        ,   ss_typename_param_k A
+        >
+inline
+basic_simple_string<C, T, A>::basic_simple_string(class_type&& rhs) STLSOFT_NOEXCEPT
+    : m_buffer(rhs.m_buffer)
+{
+    rhs.m_buffer = ss_nullptr_k;
+}
+#endif /* STLSOFT_CF_RVALUE_REFERENCES_SUPPORT */
 
 template<   ss_typename_param_k C
         ,   ss_typename_param_k T
