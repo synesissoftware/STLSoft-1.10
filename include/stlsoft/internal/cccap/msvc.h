e335ae501fe8ab6715c8d2ba4cbe27749b826aaf
Matt Wilson <matthew@synesis.com.au>
2020-11-24 17:36:38 +1100

    + detection of move semantics (for VC++ only for now)

--- include/stlsoft/internal/cccap/msvc.h
+++ include/stlsoft/internal/cccap/msvc.h
@@ -4,13 +4,14 @@
  * Purpose:     Compiler feature discrimination for Visual C++.
  *
  * Created:     7th February 2003
- * Updated:     6th September 2019
+ * Updated:     23rd November 2020
  *
  * Thanks:      To Cláudio Albuquerque for working on the
  *              Win64-compatibility.
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2003-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -23,9 +24,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -65,9 +67,9 @@
 
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_MAJOR     3
-# define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_MINOR     32
-# define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_REVISION  2
-# define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_EDIT      144
+# define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_MINOR     33
+# define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_REVISION  1
+# define STLSOFT_VER_H_STLSOFT_CCCAP_MSVC_EDIT      146
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -413,10 +415,22 @@
 # define STLSOFT_CF_STATIC_ARRAY_SIZE_DETERMINATION_SUPPORT
 #endif /* compiler */
 
+/* Move semantics, old and new
+ *
+ * STLSOFT_CF_MOVE_CONSTRUCTOR_SUPPORT is the legacy, circa-2000
+ * functionality. It is deprecated, and should not be used in any new
+ * code
+ *
+ * STLSOFT_CF_MOVE_SEMANTICS_SUPPORT is the C++11 symbol
+ */
 #if defined(_MSC_EXTENSIONS) && \
     _MSC_VER < 1310
 # define STLSOFT_CF_MOVE_CONSTRUCTOR_SUPPORT
 #endif /* _MSC_EXTENSIONS && _MSC_VER < 1310 */
+
+#if _MSC_VER >= 1700
+# define STLSOFT_CF_MOVE_SEMANTICS_SUPPORT
+#endif
 
 #if _MSC_VER >= 1300
 # define STLSOFT_CF_OPERATOR_BOOL_AS_OPERATOR_POINTER_TO_MEMBER_SUPPORT
