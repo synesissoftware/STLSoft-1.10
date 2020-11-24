43aa51ee049f3b10e810391bcd0c8a53c81939f1
Matt Wilson <matthew@synesis.com.au>
2020-11-24 17:17:45 +1100

    + added SAS-like conversion functions taking a time format string

--- include/stlsoft/shims/access/string/std/time.hpp
+++ include/stlsoft/shims/access/string/std/time.hpp
@@ -4,10 +4,11 @@
  * Purpose:     String shims for standard time structures.
  *
  * Created:     25th July 2005
- * Updated:     2nd February 2019
+ * Updated:     29th October 2020
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2005-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -20,9 +21,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -51,9 +53,9 @@
 
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_MAJOR     3
-# define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_MINOR     0
-# define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_REVISION  1
-# define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_EDIT      32
+# define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_MINOR     1
+# define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_REVISION  0
+# define STLSOFT_VER_STLSOFT_SHIMS_ACCESS_STRING_STD_HPP_TIME_EDIT      37
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -94,18 +96,20 @@ namespace stlsoft
  * All the struct tm-related conversions assume a format of 20 characters
  */
 
+/* struct tm const* */
+
 /** \ref group__concept__Shim__string_access__c_str_data function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, non-mutating pointer to a C-style
- *   string of <code>char</code>.
+ * \return None-<code>NULL</code>, non-mutating pointer to a C-style string
+ *   of <code>char</code>.
  */
 inline
 basic_shim_string<ss_char_a_t>
 c_str_data_a(
-  struct tm const* t
+    struct tm const* t
 )
 {
     typedef basic_shim_string<ss_char_a_t>  shim_string_t;
@@ -116,14 +120,13 @@ c_str_data_a(
     }
     else
     {
-        ss_char_a_t         fmt[101];
-        ss_size_t const     n0  =   STLSOFT_NS_GLOBAL(strftime)(&fmt[0], STLSOFT_NUM_ELEMENTS(fmt), "%a %b %%d %%H:%%M:%%S %%Y", t);
+        ss_char_a_t     fmt[101];
+        ss_size_t const n0  =   STLSOFT_NS_GLOBAL(strftime)(&fmt[0], STLSOFT_NUM_ELEMENTS(fmt), "%a %b %%d %%H:%%M:%%S %%Y", t);
 
         if(0 != n0)
         {
-            shim_string_t       s(n0 + 2);
-
-            ss_size_t const     n1  =   STLSOFT_NS_GLOBAL(strftime)(s.data(), 1 + s.size(), fmt, t);
+            shim_string_t   s(n0 + 2);
+            ss_size_t const n1  =   STLSOFT_NS_GLOBAL(strftime)(s.data(), 1 + s.size(), fmt, t);
 
             if(0 != n1)
             {
@@ -139,22 +142,66 @@ c_str_data_a(
     return shim_string_t("(invalid time)");
 }
 
+/* SAS-like conversion function taking time format parameter
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \param t Pointer to time value to be converted
+ * \param fmt Custom format string. Must not be \c null
+ *
+ * \return None-<code>NULL</code>, non-mutating pointer to a C-style string
+ *   of <code>char</code>.
+ */
+inline
+basic_shim_string<ss_char_a_t>
+c_str_data_a(
+    struct tm const*    t
+,   ss_char_a_t const*  fmt
+)
+{
+    STLSOFT_ASSERT(NULL != fmt);
+
+    typedef basic_shim_string<ss_char_a_t>  shim_string_t;
+
+    if(NULL == t)
+    {
+        return shim_string_t();
+    }
+    else
+    {
+        ss_size_t const n   =   c_str_len_a(fmt);
+        shim_string_t   s(2 + 7 * n);
+        ss_size_t const n1  =   STLSOFT_NS_GLOBAL(strftime)(s.data(), 1 + s.size(), fmt, t);
+
+        if(0 != n1)
+        {
+            STLSOFT_ASSERT(n1 <= s.size());
+
+            s.truncate(n1);
+
+            return s;
+        }
+    }
+
+    return shim_string_t("(invalid time)");
+}
+
 /** \ref group__concept__Shim__string_access__c_str_data function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, non-mutating pointer to a C-style string.
+ * \return None-<code>NULL</code>, non-mutating pointer to a C-style string
+ *   of <code>char</code>.
  */
 inline
 basic_shim_string<ss_char_a_t>
 c_str_data(
-  struct tm const* t
+    struct tm const* t
 )
 {
     return c_str_data_a(t);
 }
-
 
 
 /** \ref group__concept__Shim__string_access__c_str_len function
@@ -162,7 +209,7 @@ c_str_data(
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Length (in bytes) of the string <code>s</code>.
+ * \return Length of the string form of <code>t</code>.
  */
 inline
 ss_size_t
@@ -176,8 +223,8 @@ c_str_len_a(
     }
     else
     {
-        ss_char_a_t         sz[101];
-        ss_size_t const     n1  =   STLSOFT_NS_GLOBAL(strftime)(&sz[0], STLSOFT_NUM_ELEMENTS(sz), "%a %b", t);
+        ss_char_a_t     sz[101];
+        ss_size_t const n1  =   STLSOFT_NS_GLOBAL(strftime)(&sz[0], STLSOFT_NUM_ELEMENTS(sz), "%a %b", t);
 
         if(0 != n1)
         {
@@ -189,31 +236,94 @@ c_str_len_a(
     }
 }
 
+/* SAS-like conversion function taking time format parameter
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \param t Pointer to time value to be converted
+ * \param fmt Custom format string. Must not be \c null
+ *
+ * \return Length of the string form of <code>t</code>.
+ */
+inline
+ss_size_t
+c_str_len_a(
+    struct tm const*    t
+,   ss_char_a_t const*  fmt
+)
+{
+    STLSOFT_ASSERT(NULL != fmt);
+
+    if(NULL == t)
+    {
+        return 0;
+    }
+    else
+    {
+        ss_char_a_t     sz[101];
+        ss_size_t const n1  =   STLSOFT_NS_GLOBAL(strftime)(&sz[0], STLSOFT_NUM_ELEMENTS(sz), fmt, t);
+
+        if(0 != n1)
+        {
+            return n1;
+        }
+
+        return 14;
+    }
+}
+
 /** \ref group__concept__Shim__string_access__c_str_len function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Length of the string <code>s</code>.
+ * \return Length of the string form of <code>t</code>.
  */
-inline ss_size_t c_str_len(struct tm const* t)
+inline
+ss_size_t
+c_str_len(
+    struct tm const* t
+)
 {
     return c_str_len_a(t);
 }
 
 
-
 /** \ref group__concept__Shim__string_access__c_str_ptr function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, nul-terminated, non-mutating pointer to a C-style
- *   string of <code>char</code>.
+ * \return None-<code>NULL</code>, <code>nul</code>-terminated, non-mutating
+ *   pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr_a(struct tm const* t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_a(
+    struct tm const* t
+)
 {
     return c_str_data_a(t);
+}
+
+/* SAS-like conversion function taking time format parameter
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \param t Pointer to time value to be converted
+ * \param fmt Custom format string. Must not be \c null
+ *
+ * \return None-<code>NULL</code>, <code>nul</code>-terminated, non-mutating
+ *   pointer to a C-style string of <code>char</code>.
+ */
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_a(
+    struct tm const*    t
+,   ss_char_a_t const*  fmt
+)
+{
+    return c_str_data_a(t, fmt);
 }
 
 /** \ref group__concept__Shim__string_access__c_str_ptr function
@@ -221,14 +331,17 @@ inline basic_shim_string<ss_char_a_t> c_str_ptr_a(struct tm const* t)
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, nul-terminated, non-mutating pointer to a C-style
- *   string.
+ * \return None-<code>NULL</code>, <code>nul</code>-terminated, non-mutating
+ *   pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr(struct tm const* t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr(
+    struct tm const* t
+)
 {
     return c_str_data_a(t);
 }
-
 
 
 /** \ref group__concept__Shim__string_access__c_str_ptr_null function
@@ -236,10 +349,14 @@ inline basic_shim_string<ss_char_a_t> c_str_ptr(struct tm const* t)
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Possibly NULL, nul-terminated, non-mutating pointer to a C-style
- *   string of <code>char</code>.
+ * \return Possibly <code>NULL</code>, <code>nul</code>-terminated,
+ *   non-mutating pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr_null_a(struct tm const* t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_null_a(
+    struct tm const* t
+)
 {
     return c_str_data_a(t);
 }
@@ -249,55 +366,72 @@ inline basic_shim_string<ss_char_a_t> c_str_ptr_null_a(struct tm const* t)
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Possibly NULL, nul-terminated, non-mutating pointer to a C-style
- *   string.
+ * \return Possibly <code>NULL</code>, <code>nul</code>-terminated,
+ *   non-mutating pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr_null(struct tm const* t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_null(
+    struct tm const* t
+)
 {
     return c_str_data_a(t);
 }
 
-
-
-
-
+/* struct tm const& */
 
 /** \ref group__concept__Shim__string_access__c_str_data function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, non-mutating pointer to a C-style
- *   string of <code>char</code>.
+ * \return None-<code>NULL</code>, non-mutating pointer to a C-style string
+ *   of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_data(struct tm const& t)
-{
-    return c_str_data(&t);
-}
-
-/** \ref group__concept__Shim__string_access__c_str_data function
- *    for <code>struct tm</code>.
- *
- * \ingroup group__concept__Shim__string_access
- *
- * \return None-NULL, non-mutating pointer to a C-style string.
- */
-inline basic_shim_string<ss_char_a_t> c_str_data_a(struct tm const& t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_data_a(
+    struct tm const& t
+)
 {
     return c_str_data_a(&t);
 }
 
+/* SAS-like conversion function taking time format parameter
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \param t Time value to be converted
+ * \param fmt Custom format string. Must not be \c null
+ *
+ * \return None-<code>NULL</code>, non-mutating pointer to a C-style string
+ *   of <code>char</code>.
+ */
+inline
+basic_shim_string<ss_char_a_t>
+c_str_data_a(
+    struct tm const&    t
+,   ss_char_a_t const*  fmt
+)
+{
+    return c_str_data_a(&t, fmt);
+}
 
-/** \ref group__concept__Shim__string_access__c_str_len function
+/** \ref group__concept__Shim__string_access__c_str_data function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Length (in bytes) of the string <code>s</code>.
+ * \return None-<code>NULL</code>, non-mutating pointer to a C-style string
+ *   of <code>char</code>.
  */
-inline ss_size_t c_str_len_a(struct tm const& t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_data(
+    struct tm const& t
+)
 {
-    return c_str_len_a(&t);
+    return c_str_data(&t);
 }
 
 /** \ref group__concept__Shim__string_access__c_str_len function
@@ -305,9 +439,48 @@ inline ss_size_t c_str_len_a(struct tm const& t)
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Length of the string <code>s</code>.
+ * \return Length of the string form of <code>t</code>.
  */
-inline ss_size_t c_str_len(struct tm const& t)
+inline
+ss_size_t
+c_str_len_a(
+    struct tm const& t
+)
+{
+    return c_str_len_a(&t);
+}
+
+/* SAS-like conversion function taking time format parameter
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \param t Pointer to time value to be converted
+ * \param fmt Custom format string. Must not be \c null
+ *
+ * \return Length of the string form of <code>t</code>.
+ */
+inline
+ss_size_t
+c_str_len_a(
+    struct tm const&    t
+,   ss_char_a_t const*  fmt
+)
+{
+    return c_str_len_a(&t, fmt);
+}
+
+/** \ref group__concept__Shim__string_access__c_str_len function
+ *    for <code>struct tm</code>.
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \return Length of the string form of <code>t</code>.
+ */
+inline
+ss_size_t
+c_str_len(
+    struct tm const& t
+)
 {
     return c_str_len(&t);
 }
@@ -318,22 +491,51 @@ inline ss_size_t c_str_len(struct tm const& t)
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, nul-terminated, non-mutating pointer to a C-style
- *   string of <code>char</code>.
+ * \return None-<code>NULL</code>, <code>nul</code>-terminated, non-mutating
+ *   pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr_a(struct tm const& t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_a(
+    struct tm const& t
+)
 {
     return c_str_ptr_a(&t);
 }
+
+/* SAS-like conversion function taking time format parameter
+ *
+ * \ingroup group__concept__Shim__string_access
+ *
+ * \param t Pointer to time value to be converted
+ * \param fmt Custom format string. Must not be \c null
+ *
+ * \return None-NULL, nul-terminated, non-mutating pointer to a C-style
+ *   string of <code>char</code>.
+ */
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_a(
+    struct tm const&    t
+,   ss_char_a_t const*  fmt
+)
+{
+    return c_str_data_a(&t, fmt);
+}
+
 /** \ref group__concept__Shim__string_access__c_str_ptr function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return None-NULL, nul-terminated, non-mutating pointer to a C-style
- *   string.
+ * \return None-<code>NULL</code>, <code>nul</code>-terminated, non-mutating
+ *   pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr(struct tm const& t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr(
+    struct tm const& t
+)
 {
     return c_str_ptr(&t);
 }
@@ -344,22 +546,31 @@ inline basic_shim_string<ss_char_a_t> c_str_ptr(struct tm const& t)
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Possibly NULL, nul-terminated, non-mutating pointer to a C-style
- *   string of <code>char</code>.
+ * \return Possibly <code>NULL</code>, <code>nul</code>-terminated,
+ *   non-mutating pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr_null_a(struct tm const& t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_null_a(
+    struct tm const& t
+)
 {
     return c_str_ptr_null_a(&t);
 }
+
 /** \ref group__concept__Shim__string_access__c_str_ptr_null function
  *    for <code>struct tm</code>.
  *
  * \ingroup group__concept__Shim__string_access
  *
- * \return Possibly NULL, nul-terminated, non-mutating pointer to a C-style
- *   string.
+ * \return Possibly <code>NULL</code>, <code>nul</code>-terminated,
+ *   non-mutating pointer to a C-style string of <code>char</code>.
  */
-inline basic_shim_string<ss_char_a_t> c_str_ptr_null(struct tm const& t)
+inline
+basic_shim_string<ss_char_a_t>
+c_str_ptr_null(
+    struct tm const& t
+)
 {
     return c_str_ptr_null(&t);
 }
