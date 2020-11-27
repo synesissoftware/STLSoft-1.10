381d4b68e7894318c14234771cc46d8f2a522318
Matt Wilson <matthew@synesis.com.au>
2020-11-27 11:42:20 +1100

    ~ spacing

--- include/atlstl/automation/property_method_helpers.hpp
+++ include/atlstl/automation/property_method_helpers.hpp
@@ -5,10 +5,11 @@
  *              property methods of ATL COM server classes.
  *
  * Created:     25th June 2002
- * Updated:     13th September 2019
+ * Updated:     25th November 2020
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2002-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -21,9 +22,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -55,7 +57,7 @@
 # define ATLSTL_VER_ATLSTL_AUTOMATION_HPP_PROPERTY_METHOD_HELPERS_MAJOR    5
 # define ATLSTL_VER_ATLSTL_AUTOMATION_HPP_PROPERTY_METHOD_HELPERS_MINOR    0
 # define ATLSTL_VER_ATLSTL_AUTOMATION_HPP_PROPERTY_METHOD_HELPERS_REVISION 2
-# define ATLSTL_VER_ATLSTL_AUTOMATION_HPP_PROPERTY_METHOD_HELPERS_EDIT     87
+# define ATLSTL_VER_ATLSTL_AUTOMATION_HPP_PROPERTY_METHOD_HELPERS_EDIT     88
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -274,7 +276,7 @@ get_MemberValue(
 ,   size_t  C::*mem
 )
 {
-    if(NULL == ret)
+    if (NULL == ret)
     {
         return E_POINTER;
     }
@@ -282,7 +284,7 @@ get_MemberValue(
     {
         size_t const v = pThis->*mem;
 
-        if(size_t(integral_limits<long>::maximum()) < v)
+        if (size_t(integral_limits<long>::maximum()) < v)
         {
             return DISP_E_OVERFLOW;
         }
@@ -324,11 +326,14 @@ inline HRESULT get_MemberValue(C* const cls, T* ret, T2 (C::*pfn)() const)
 
 
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
-#if defined(STLSOFT_COMPILER_IS_MSVC) && \
-    _MSC_VER >= 1400
-# pragma message("try this in VC++ 8+")
-#endif
-#if 0
+
+# if defined(STLSOFT_COMPILER_IS_MSVC) && \
+     _MSC_VER >= 1400
+
+#  pragma message("try this in VC++ 8+")
+# endif
+# if 0
+
 /* Overloads for specific mismatches between interface and method types*/
 
 /*
@@ -344,7 +349,7 @@ get_MemberValue(
 ,   size_t (C::*pfn)() const
 )
 {
-    if(NULL == ret)
+    if (NULL == ret)
     {
         return E_POINTER;
     }
@@ -352,7 +357,7 @@ get_MemberValue(
     {
         size_t const v = (pThis->*pfn)();
 
-        if(size_t(integral_limits<long>::maximum()) < v)
+        if (size_t(integral_limits<long>::maximum()) < v)
         {
             return DISP_E_OVERFLOW;
         }
@@ -364,11 +369,8 @@ get_MemberValue(
         }
     }
 }
-#endif
+# endif
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
-
-
-
 
 /* /////////////////////////////////////////////////////////////////////////
  * put_MemberValue
