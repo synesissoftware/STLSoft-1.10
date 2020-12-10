ce76b71dbb0e4e41ec267c28c77ce49e8928402b
Matt Wilson <matthew@synesis.com.au>
2020-12-10 11:32:12 +1100

    unixstl console_functions.h : ~ now behaves correctly when simulating UNIX on Windows

--- include/unixstl/system/console_functions.h
+++ include/unixstl/system/console_functions.h
@@ -4,10 +4,11 @@
  * Purpose:     Windows console functions.
  *
  * Created:     6th August 2015
- * Updated:     2nd February 2019
+ * Updated:     2nd December 2020
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2015-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -20,9 +21,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the names of Matthew Wilson and Synesis Software nor the names
- *   of any contributors may be used to endorse or promote products derived
- *   from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -51,8 +53,8 @@
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define UNIXSTL_VER_UNIXSTL_SYSTEM_H_CONSOLE_FUNCTIONS_MAJOR       1
 # define UNIXSTL_VER_UNIXSTL_SYSTEM_H_CONSOLE_FUNCTIONS_MINOR       0
-# define UNIXSTL_VER_UNIXSTL_SYSTEM_H_CONSOLE_FUNCTIONS_REVISION    6
-# define UNIXSTL_VER_UNIXSTL_SYSTEM_H_CONSOLE_FUNCTIONS_EDIT        10
+# define UNIXSTL_VER_UNIXSTL_SYSTEM_H_CONSOLE_FUNCTIONS_REVISION    7
+# define UNIXSTL_VER_UNIXSTL_SYSTEM_H_CONSOLE_FUNCTIONS_EDIT        11
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -93,10 +95,16 @@
 # include <io.h>
 #endif
 
-#if defined(_WIN32)
+#if 0
+#elif defined(_WIN32) && \
+      defined(_STLSOFT_FORCE_ANY_COMPILER)
+# ifndef WINSTL_INCL_WINSTL_API_H_winstl_win32_winnt_
+#  include <winstl/api/winstl_win32_winnt_.h>
+# endif /* !WINSTL_INCL_WINSTL_API_H_winstl_win32_winnt_ */
 # ifndef WINSTL_INCL_WINSTL_API_external_h_Console
 #  include <winstl/api/external/Console.h>
 # endif /* !WINSTL_INCL_WINSTL_API_external_h_Console */
+# include <windows.h>
 #endif
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -137,27 +145,48 @@ unixstl_C_get_console_width(void)
      *
      */
 
-    int const fn_stdout = STLSOFT_NS_GLOBAL(fileno)(stdout);
+    int (*pfn_fileno)(FILE*);
+    int (*pfn_isatty)(int);
+    char* (*pfn_getenv)(char const*);
+
+#if defined(_MSC_VER) && \
+    defined(_STLSOFT_FORCE_ANY_COMPILER)
+
+# if _MSC_VER >= 1200
+#  pragma warning(push)
+#  pragma warning(disable : 4996)
+# endif /* compiler */
+
+    pfn_fileno  =   STLSOFT_NS_GLOBAL(_fileno);
+    pfn_isatty  =   STLSOFT_NS_GLOBAL(_isatty);
+    pfn_getenv  =   STLSOFT_NS_GLOBAL(getenv);
+
+# if _MSC_VER >= 1200
+#  pragma warning(pop)
+# endif /* compiler */
+#else
+
+    pfn_fileno  =   STLSOFT_NS_GLOBAL(fileno);
+    pfn_isatty  =   STLSOFT_NS_GLOBAL(isatty);
+    pfn_getenv  =   STLSOFT_NS_GLOBAL(getenv);
+#endif
+
+    int const fn_stdout = (*pfn_fileno)(stdout);
 
 
 
     /* 1. Try the environment variable COLUMNS, if we're a tty */
 
-#if defined(_MSC_VER) && \
-    defined(_STLSOFT_FORCE_ANY_COMPILER)
-    if(STLSOFT_NS_GLOBAL(_isatty(fn_stdout)))
-#else
-    if(STLSOFT_NS_GLOBAL(isatty(fn_stdout)))
-#endif
+    if ((*pfn_isatty)(fn_stdout))
     {
-        char const* const columns = STLSOFT_NS_GLOBAL(getenv)("COLUMNS");
+        char const* const columns = (*pfn_getenv)("COLUMNS");
 
-        if(NULL != columns)
+        if (NULL != columns)
         {
             char *      ep  =   NULL;
             long const  n   =   STLSOFT_NS_GLOBAL(strtol)(columns, &ep, 10);
 
-            if( NULL != ep &&
+            if (NULL != ep &&
                 '\0' == *ep)
             {
                 return STLSOFT_STATIC_CAST(us_size_t, n);
@@ -166,13 +195,31 @@ unixstl_C_get_console_width(void)
     }
 
 
-#if !defined(_WIN32)
+#if 0
+#elif defined(_WIN32) && \
+      defined(_STLSOFT_FORCE_ANY_COMPILER)
+
+    /* 2. get x from Windows' screen buffer */
+    {
+        HANDLE hStdOut = WINSTL_API_EXTERNAL_Console_GetStdHandle(STD_OUTPUT_HANDLE);
+
+        if (INVALID_HANDLE_VALUE != hStdOut)
+        {
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+            if (WINSTL_API_EXTERNAL_Console_GetConsoleScreenBufferInfo(hStdOut, &csbi))
+            {
+                return csbi.dwMaximumWindowSize.X;
+            }
+        }
+    }
+#else
 
     /* 2. get TIOCGWINSZ from ioctl */
     {
         struct winsize ws;
 
-        if(-1 != STLSOFT_NS_GLOBAL(ioctl)(STDOUT_FILENO, TIOCGWINSZ, &ws))
+        if (-1 != STLSOFT_NS_GLOBAL(ioctl)(STDOUT_FILENO, TIOCGWINSZ, &ws))
         {
             return STLSOFT_STATIC_CAST(us_size_t, ws.ws_col);
         }
