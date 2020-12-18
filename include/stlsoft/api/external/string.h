a435f3e81616d4a9729cce247f887f7f1699f734
Matt Wilson <matthew@synesis.com.au>
2020-12-18 12:50:44 +1100

    ~ preparatory changes

--- include/stlsoft/api/external/string.h
+++ include/stlsoft/api/external/string.h
@@ -4,10 +4,11 @@
  * Purpose:     External preprocessor aliases for string functions.
  *
  * Created:     15th November 2002
- * Updated:     13th September 2019
+ * Updated:     17th December 2020
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2002-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -20,9 +21,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -92,14 +94,17 @@
 
 # if 0
 # elif defined(__STDC__)
+
 #  define STLSOFT_API_EXTERNAL_string_USE_MSVC_COMPILER_EXTENSION_FORMS_
-# elif 1&&\
+# elif 1 &&\
        _MSC_VER >= 1500 &&\
        !defined(_CRT_SECURE_NO_WARNINGS) &&\
        !defined(_CRT_NONSTDC_NO_WARNINGS) &&\
        1
+
 #  define STLSOFT_API_EXTERNAL_string_USE_MSVC_COMPILER_EXTENSION_FORMS_
 # else
+
 # endif
 
 #else
