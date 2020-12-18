c32e1c947647e3d67561dac5f8e6546e21ef2f60
Matt Wilson <matthew@synesis.com.au>
2020-12-18 12:53:32 +1100

    ~ removing dependency on deprecated constructs

--- include/unixstl/filesystem/pipe.hpp
+++ include/unixstl/filesystem/pipe.hpp
@@ -4,10 +4,11 @@
  * Purpose:     pipe class, based on UNIX pipe.
  *
  * Created:     19th June 2004
- * Updated:     13th September 2019
+ * Updated:     18th December 2020
  *
  * Home:        http://stlsoft.org/
  *
+ * Copyright (c) 2019-2020, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2004-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -20,9 +21,10 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name(s) of Matthew Wilson and Synesis Software nor the
- *   names of any contributors may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
+ * - Neither the name(s) of Matthew Wilson and Synesis Information Systems
+ *   nor the names of any contributors may be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -51,8 +53,8 @@
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define UNIXSTL_VER_UNIXSTL_FILESYSTEM_HPP_PIPE_MAJOR      4
 # define UNIXSTL_VER_UNIXSTL_FILESYSTEM_HPP_PIPE_MINOR      1
-# define UNIXSTL_VER_UNIXSTL_FILESYSTEM_HPP_PIPE_REVISION   7
-# define UNIXSTL_VER_UNIXSTL_FILESYSTEM_HPP_PIPE_EDIT       53
+# define UNIXSTL_VER_UNIXSTL_FILESYSTEM_HPP_PIPE_REVISION   8
+# define UNIXSTL_VER_UNIXSTL_FILESYSTEM_HPP_PIPE_EDIT       54
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -151,11 +153,11 @@ public:
     {
         if(-1 != read_handle())
         {
-            ::close(m_handles[0]);
+            close_(m_handles[0]);
         }
         if(-1 != write_handle())
         {
-            ::close(m_handles[1]);
+            close_(m_handles[1]);
         }
     }
 /// @}
@@ -182,7 +184,7 @@ public:
     {
         if(-1 != read_handle())
         {
-            ::close(m_handles[0]);
+            close_(m_handles[0]);
             m_handles[0] = -1;
         }
     }
@@ -191,7 +193,7 @@ public:
     {
         if(-1 != write_handle())
         {
-            ::close(m_handles[1]);
+            close_(m_handles[1]);
             m_handles[1] = -1;
         }
     }
@@ -200,6 +202,38 @@ public:
     {
         close_read();
         close_write();
+    }
+/// @}
+
+/// \name Implementation
+/// @{
+private:
+    static
+    void
+    close_(
+        int h
+    )
+    {
+#if 0
+#elif defined(STLSOFT_COMPILER_IS_MSVC)
+
+# if _MSC_VER >= 1500
+
+#  pragma warning(push)
+#  pragma warning(disable : 4996)
+# endif
+#endif
+
+        ::close(h);
+
+#if 0
+#elif defined(STLSOFT_COMPILER_IS_MSVC)
+
+# if _MSC_VER >= 1500
+
+#  pragma warning(pop)
+# endif
+#endif
     }
 /// @}
 
