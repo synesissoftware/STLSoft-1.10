f66137d6dee2390669c6ccfc57af34fc921b18ed
Matt Wilson <matthew@synesis.com.au>
2020-12-30 16:30:41 +1100

    + added operators with lhs as cc* and rhs as slice const& + added noexcept

--- include/stlsoft/string/string_slice.hpp
+++ include/stlsoft/string/string_slice.hpp
@@ -4,7 +4,7 @@
  * Purpose:     Defines the string_slice class template.
  *
  * Created:     22nd February 2010
- * Updated:     3rd December 2020
+ * Updated:     28th December 2020
  *
  * Home:        http://stlsoft.org/
  *
@@ -52,9 +52,9 @@
 
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_MAJOR      1
-# define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_MINOR      3
-# define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_REVISION   7
-# define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_EDIT       27
+# define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_MINOR      5
+# define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_REVISION   1
+# define STLSOFT_VER_STLSOFT_STRING_HPP_STRING_SLICE_EDIT       30
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -99,32 +99,34 @@ namespace stlsoft
  */
 
 /// A type for describing string slices
-template<   ss_typename_param_k C
-        ,   ss_typename_param_k T = stlsoft_char_traits<C>
-        ,   ss_typename_param_k P = ss_typename_type_def_k string_slice_selection_traits_t<C>::slice_type
-        >
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T = stlsoft_char_traits<C>
+,   ss_typename_param_k P = ss_typename_type_def_k string_slice_selection_traits_t<C>::slice_type
+>
 struct string_slice
     : public P
 {
 public: // Member Types
     /// The character type
-    typedef C                                       char_type;
+    typedef C                                               char_type;
     /// The traits type
-    typedef T                                       traits_type;
+    typedef T                                               traits_type;
 private:
-    typedef P                                       parent_class_type;
+    typedef P                                               parent_class_type;
 public:
     /// The size type
-    typedef ss_size_t                               size_type;
+    typedef ss_size_t                                       size_type;
     /// The class type
-    typedef string_slice<char_type, traits_type, P> class_type;
+    typedef string_slice<char_type, traits_type, P>         class_type;
     /// The boolean type
-    typedef ss_bool_t                               bool_type;
+    typedef ss_bool_t                                       bool_type;
     /// The comparison type
-    typedef int                                     int_type;
-
-    typedef C                                       value_type;
-    typedef C const*                                const_iterator;
+    typedef int                                             int_type;
+    /// The value type
+    typedef C                                               value_type;
+    /// The non-mutating (const) iterator type
+    typedef C const*                                        const_iterator;
 
 public: // Construction
     /// Default constructor
@@ -165,20 +167,20 @@ public: // Construction
 public: // Comparison
     /// Determines whether the current instance is equal to the given
     /// instance
-    bool_type equals(class_type const& rhs) const
+    bool_type equals(class_type const& rhs) const STLSOFT_NOEXCEPT
     {
         return this->len == rhs.len && (this->ptr == rhs.ptr || 0 == traits_type::compare(this->ptr, rhs.ptr, this->len));
     }
 
     /// Determines whether the current instance is equal to the given
     /// string
-    bool_type equals(char_type const* s) const
+    bool_type equals(char_type const* s) const STLSOFT_NOEXCEPT
     {
         return equals(class_type(s));
     }
 
     /// Compares the current instance with the given instance
-    int_type compare(class_type const& rhs) const
+    int_type compare(class_type const& rhs) const STLSOFT_NOEXCEPT
     {
         size_type   n   =   (this->len < rhs.len) ? this->len : rhs.len;
         int_type    r   =   traits_type::compare(this->ptr, rhs.ptr, n);
@@ -193,7 +195,7 @@ public: // Comparison
     }
 
     /// Compares the current instance with the given string
-    int_type compare(char_type const* s) const
+    int_type compare(char_type const* s) const STLSOFT_NOEXCEPT
     {
         return compare(class_type(s));
     }
@@ -203,14 +205,14 @@ public: // Iteration
     /// Begins the iteration
     ///
     /// \return A non-mutable (const) iterator representing the start of the sequence
-    const_iterator          begin() const
+    const_iterator          begin() const STLSOFT_NOEXCEPT
     {
         return this->ptr;
     }
     /// Ends the iteration
     ///
     /// \return A non-mutable (const) iterator representing the end of the sequence
-    const_iterator          end() const
+    const_iterator          end() const STLSOFT_NOEXCEPT
     {
         return this->ptr + this->len;
     }
@@ -220,153 +222,297 @@ public: // Iteration
  * comparison operators
  */
 
-/* slice const& */
+/* slice, slice */
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator ==(
     string_slice<C, T, P> const& lhs
 ,   string_slice<C, T, P> const& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.equals(rhs);
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator !=(
     string_slice<C, T, P> const& lhs
 ,   string_slice<C, T, P> const& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return !lhs.equals(rhs);
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator <(
     string_slice<C, T, P> const& lhs
 ,   string_slice<C, T, P> const& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) < 0;
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator >(
     string_slice<C, T, P> const& lhs
 ,   string_slice<C, T, P> const& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) > 0;
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator <=(
     string_slice<C, T, P> const& lhs
 ,   string_slice<C, T, P> const& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) <= 0;
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator >=(
     string_slice<C, T, P> const& lhs
 ,   string_slice<C, T, P> const& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) >= 0;
 }
 
-/* char_type const* */
+/* slice, char_type const* */
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator ==(
     string_slice<C, T, P> const&                               lhs
 ,   ss_typename_type_k string_slice<C, T, P>::char_type const* rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.equals(rhs);
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator !=(
     string_slice<C, T, P> const&                               lhs
 ,   ss_typename_type_k string_slice<C, T, P>::char_type const* rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return !lhs.equals(rhs);
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator <(
     string_slice<C, T, P> const&                               lhs
 ,   ss_typename_type_k string_slice<C, T, P>::char_type const* rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) < 0;
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator >(
     string_slice<C, T, P> const&                               lhs
 ,   ss_typename_type_k string_slice<C, T, P>::char_type const* rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) > 0;
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator <=(
     string_slice<C, T, P> const&                               lhs
 ,   ss_typename_type_k string_slice<C, T, P>::char_type const* rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) <= 0;
 }
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 ss_typename_type_k string_slice<C, T, P>::bool_type
 operator >=(
     string_slice<C, T, P> const&                               lhs
 ,   ss_typename_type_k string_slice<C, T, P>::char_type const* rhs
-)
+) STLSOFT_NOEXCEPT
 {
     return lhs.compare(rhs) >= 0;
+}
+
+/* char_type const*, slice */
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
+inline
+ss_typename_type_k string_slice<C, T, P>::bool_type
+operator ==(
+    ss_typename_type_k string_slice<C, T, P>::char_type const* lhs
+,   string_slice<C, T, P> const&                               rhs
+) STLSOFT_NOEXCEPT
+{
+    return rhs.equals(lhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
+inline
+ss_typename_type_k string_slice<C, T, P>::bool_type
+operator !=(
+    ss_typename_type_k string_slice<C, T, P>::char_type const* lhs
+,   string_slice<C, T, P> const&                               rhs
+) STLSOFT_NOEXCEPT
+{
+    return !rhs.equals(lhs);
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
+inline
+ss_typename_type_k string_slice<C, T, P>::bool_type
+operator <(
+    ss_typename_type_k string_slice<C, T, P>::char_type const* lhs
+,   string_slice<C, T, P> const&                               rhs
+) STLSOFT_NOEXCEPT
+{
+    return rhs.compare(lhs) > 0;
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
+inline
+ss_typename_type_k string_slice<C, T, P>::bool_type
+operator >(
+    ss_typename_type_k string_slice<C, T, P>::char_type const* lhs
+,   string_slice<C, T, P> const&                               rhs
+) STLSOFT_NOEXCEPT
+{
+    return rhs.compare(lhs) < 0;
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
+inline
+ss_typename_type_k string_slice<C, T, P>::bool_type
+operator <=(
+    ss_typename_type_k string_slice<C, T, P>::char_type const* lhs
+,   string_slice<C, T, P> const&                               rhs
+) STLSOFT_NOEXCEPT
+{
+    return rhs.compare(lhs) > 0;
+}
+
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
+inline
+ss_typename_type_k string_slice<C, T, P>::bool_type
+operator >=(
+    ss_typename_type_k string_slice<C, T, P>::char_type const* lhs
+,   string_slice<C, T, P> const&                               rhs
+) STLSOFT_NOEXCEPT
+{
+    return rhs.compare(lhs) < 0;
 }
 
 /* /////////////////////////////////////////////////////////////////////////
  * swapping
  */
 
-template <ss_typename_param_k C, ss_typename_param_k T, ss_typename_param_k P>
+template<
+    ss_typename_param_k C
+,   ss_typename_param_k T
+,   ss_typename_param_k P
+>
 inline
 void
 swap(
     string_slice<C, T, P>& lhs
 ,   string_slice<C, T, P>& rhs
-)
+) STLSOFT_NOEXCEPT
 {
     lhs.swap(rhs);
 }
